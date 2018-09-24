272ec81bd3b83b24dee3d28483c44f7c0b4fab7b
dcollins4096 <none@none>
2018-09-24 17:41:25 -0400

    UNIT TESTS WORK. MAKES A POWERLAW IN ONLY f-.  IF IT DOESNT WORK NUMPY IS BROKEN

--- unit_tests_2.py
+++ unit_tests_2.py
@@ -17,7 +17,7 @@ class miscellaneous_things():
         self.form_s = " %5s"
         self.form_f = " %5.2f"
 t=miscellaneous_things()
-def test_k_array(state=None,WRITE=False, directory ='./Run/' ):
+def test_k_array_simple(state=None,WRITE=False, directory ='./Run/' ):
     if state is None:
         state = p49_eigen.waves(hx=1.0,hy=1.41421,hz=0.5,p=0.6,this_wave="f-")
     size = 32
@@ -47,6 +47,30 @@ def test_k_array(state=None,WRITE=False, directory ='./Run/' ):
                       wave=wave, start=True,write=WRITE, single_wave_selector=False)
     these_ffts  = p49_eigen.get_ffts(state.cubes, means={})
     return state, these_ffts
-state, mask=test_k_array(directory = './Run2', WRITE=True)
-#unit_tests.test_read_ics(ic_dir="./Run2",plot_dir="./Vis2")
+def test_k_array_powers(state=None,WRITE=False, directory ='./Run/' ):
+    if state is None:
+        state = p49_eigen.waves(hx=1.0,hy=1.41421,hz=0.5,p=0.6,this_wave="f-")
+    size = 32
+    ks = p49_eigen.make_k_freqs_and_int(size)
+    k3 = ks['k_freq']
+    kint = ks['k_int']
+    kint_norm = np.sqrt( kint[0,...]**2 + kint[1,...]**2 + kint[2,...]**2 )
+    k_norm    = np.sqrt( k3[0,...]**2 + k3[1,...]**2 + k3[2,...]**2 )
+    ampl  = np.zeros_like(k_norm)*(1+0j)
+    theta = np.pi*2*np.random.random(ampl.shape)
+    phase = np.exp(theta*1j)
+    mask  = np.ones_like(kint[2,...],dtype='bool')
+    mask  = np.logical_and(mask, kint[2,...]>0)
+    ampl[mask] = k_norm[mask]**(-5./3)*phase[mask]
+    ampl[mask] *= 1e-3/ampl[mask].max()
+    
+    wave='f-'
+    state.perturb(pert_shape='fft',base_size=nar([size]*3),
+                  ampl=ampl,directory=directory,
+                  k_rot=kint,
+                      wave=wave, start=True,write=WRITE, single_wave_selector=False)
+    these_ffts  = p49_eigen.get_ffts(state.cubes, means={})
+    return state, these_ffts
+state, mask=test_k_array_powers(directory = './Run2', WRITE=True)
+unit_tests.test_read_ics(ic_dir="./Run2",plot_dir="./Vis2/")
 #unit_tests.test_read_outputs(data_dir="./Run2",plot_dir="./Vis2")
