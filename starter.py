26ab82ad8aa40d0014dd6b40a999960f94bea501
dcollins4096 <none@none>
2018-09-26 15:16:44 -0400

    adding starter

---
+++ starter.py
@@ -0,0 +1,10 @@
+
+from go_lite_pyt3 import *
+import yt
+import enzo_write
+import p49_eigen
+import p49_plot_tools
+from p49_print_tools import *
+reload(enzo_write)
+reload(p49_eigen)
+reload(p49_plot_tools)
